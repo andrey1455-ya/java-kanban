555ff1a250a60e987f4fd3928a64a82986f4f416
Andrey Pesterev <andrey1455@gmail.com>
2024-09-16 17:31:55 +0300

    Реализовать двусвязный список задач

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -3,32 +3,70 @@ package manager;
 import model.Task;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private static final int MAX_HISTORY_SIZE = 10;
-    private final LinkedList<Task> taskHistoryList = new LinkedList<>();
+    private Node<Task> head;
+    private Node<Task> tail;
+    private final Map<Integer, Node<Task>> taskHistoryMap = new HashMap<>();
 
     @Override
     public void addTaskToHistory(Task task) {
         if (task != null) {
-            if (taskHistoryList.size() < MAX_HISTORY_SIZE) {
-                taskHistoryList.add(task);
-            } else {
-                taskHistoryList.removeFirst();
-                taskHistoryList.add(task);
-            }
+            remove(task.getId());
+            linkLast(task);
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(taskHistoryList);
+        List<Task> historyList = new ArrayList<>();
+        Node<Task> currentNode = head;
+        while (currentNode != null) {
+            historyList.add(currentNode.data);
+            currentNode = currentNode.next;
+        }
+        return historyList;
     }
 
     @Override
     public void remove(int id) {
+        Node<Task> node = taskHistoryMap.get(id);
+        taskHistoryMap.remove(id);
+        if (node != null) {
+            removeNode(node);
+        }
+    }
 
+    private void linkLast(Task task) {
+        final Node<Task> oldTail = tail;
+        final Node<Task> newNode = new Node<>(oldTail, task, null);
+        tail = newNode;
+        if (oldTail == null) {
+            head = newNode;
+        } else {
+            oldTail.next = newNode;
+        }
+        taskHistoryMap.put(task.getId(), newNode);
+    }
+
+    private void removeNode(Node<Task> node) {
+        final Node<Task> prevNode = node.prev;
+        final Node<Task> nextNode = node.next;
+        if (prevNode == null) {
+            head = nextNode;
+        } else {
+            prevNode.next = nextNode;
+            node.prev = null;
+        }
+        if (nextNode == null) {
+            tail = prevNode;
+        } else {
+            nextNode.prev = prevNode;
+            node.next = null;
+        }
+        node.data = null;
     }
 }
