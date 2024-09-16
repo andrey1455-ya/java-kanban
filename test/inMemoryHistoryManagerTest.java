0842ca449acfc0aefa7f536d5715575830e53785
Andrey Pesterev <andrey1455@gmail.com>
2024-09-16 20:42:45 +0300

    Правки тестов

--- test/inMemoryHistoryManagerTest.java
+++ test/inMemoryHistoryManagerTest.java
@@ -14,32 +14,7 @@ public class inMemoryHistoryManagerTest {
     private TaskManager inMemoryTaskManager = Managers.getDefault();
 
     @Test
-    public void shouldBeTasksAddedToHistory() {//Задачи, добавляемые в HistoryManager, сохраняют предыдущую версию задачи и её данных.
-        //prepare
-        Task task1 = new Task(1, "Task 1", "Description 1");
-        Epic epic1 = new Epic(1, "Epic 1", "Epic 1 Description");
-        Subtask subtask1 = new Subtask(1, 2, "Subtask1", "Subtask 1 Description", Status.NEW);
-        //do
-        inMemoryTaskManager.addNewTask(task1);
-        inMemoryTaskManager.addNewEpic(epic1);
-        inMemoryTaskManager.addNewSubtask(subtask1);
-        inMemoryTaskManager.getTaskById(1);
-        inMemoryTaskManager.getEpicById(2);
-        inMemoryTaskManager.getSubtaskById(3);
-        List<Task> history = inMemoryTaskManager.getHistory();
-        //check
-        assertEquals(task1, history.get(0), "Таска в истории не соответствует вызванной");
-        assertEquals(epic1, history.get(1), "Эпик в истории не соответствует вызванному");
-        assertEquals(subtask1, history.get(2), "Сабтаска в истории не соответствует вызванной");
-        task1.setStatus(Status.IN_PROGRESS);
-        inMemoryTaskManager.getTaskById(1);
-        history = inMemoryTaskManager.getHistory();
-        assertEquals(task1, history.get(3), "Таска в истории не соответствует вызванной");
-    }
-
-
-    @Test
-    public void historyManagerShouldPutCurrentTasks() {
+    public void shouldBeTasksAddedToHistory() {
         //prepare
         Task task1 = new Task(1, "Task 1", "Task 1 Description");
         Epic epic1 = new Epic(1, "Epic 1", "Epic 1 Description");
